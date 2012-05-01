78a425b89f1b925c01127fd5f2acac639f868ce0
apoloeam@gmail.com <apoloeam@gmail.com@ed173650-50d6-6ff3-37b3-900c6ac7dc41>
2012-05-01 22:58:47 +0000

    bean

---
+++ trunk/clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/InventarioBean.java
@@ -0,0 +1,62 @@
+package edu.eam.clinica.web.bean.funcionario;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import edu.eam.clinica.jpa.entidades.Inventario;
+import edu.eam.clinica.jpa.entidades.Medicamento;
+import edu.eam.clinica.jpa.enumeraciones.GrupoTerapeuticoEnum;
+import edu.eam.clinica.jpa.enumeraciones.PresentacionEnum;
+import edu.eam.clinica.jpa.utilidades.FactoryEntityManager;
+
+public class InventarioBean  {
+
+	
+	private GrupoTerapeuticoEnum grupoTerapeutico;
+	private String registroInvima;
+	private PresentacionEnum presentacion;
+	private EntityManager em;
+	
+	public InventarioBean(){
+		
+		em=FactoryEntityManager.getEm();
+		
+	}
+	
+	public List<Medicamento> getMedicamentos(){
+		
+		return em.createNamedQuery("MedicamentosAll").getResultList();
+		
+	}
+	
+	
+
+	public GrupoTerapeuticoEnum getGrupoTerapeutico() {
+		return grupoTerapeutico;
+	}
+
+	public void setGrupoTerapeutico(GrupoTerapeuticoEnum grupoTerapeutico) {
+		this.grupoTerapeutico = grupoTerapeutico;
+	}
+
+	public String getRegistroInvima() {
+		return registroInvima;
+	}
+
+	public void setRegistroInvima(String registroInvima) {
+		this.registroInvima = registroInvima;
+	}
+
+	public PresentacionEnum getPresentacion() {
+		return presentacion;
+	}
+
+	public void setPresentacion(PresentacionEnum presentacion) {
+		this.presentacion = presentacion;
+	}
+	
+	
+	
+	
+}
