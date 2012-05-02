adebdd83fdc5c33370d9eefe41ceb41a95dd9f74
davidagb20 <davidagb20@gmail.com>
2012-05-02 04:51:19 +0000

    Asignacion de citas, interfaz, internacionalizacion, AsignarCitaBean, managedbean.

---
+++ clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/AsignarCitasBean.java
@@ -0,0 +1,102 @@
+package edu.eam.clinica.web.bean.funcionario;
+
+import java.sql.Date;
+import java.util.*;
+
+import javax.persistence.EntityManager;
+
+import edu.eam.clinica.jpa.entidades.Medico;
+import edu.eam.clinica.jpa.utilidades.FactoryEntityManager;
+
+public class AsignarCitasBean {
+
+	private int nroConsulta;
+	private String cedulaPaciente;
+	private String medico;
+	private Date fechaCita;
+	private String motivoConsulta;
+	private String procedimientoConsulta;
+	private boolean unidad;
+	private String estadoConsulta;
+	
+	private EntityManager em;
+	
+	public AsignarCitasBean(){
+		em = FactoryEntityManager.getEm();
+	}
+	
+
+	public List<Medico> getMedicos(){
+		return em.createNamedQuery("Medico.Medico.findAll").getResultList();
+	}
+	
+	public String crearCrita(){
+		
+		return null;
+	}
+
+	public int getNroConsulta() {
+		return nroConsulta;
+	}
+
+	public void setNroConsulta(int nroConsulta) {
+		this.nroConsulta = nroConsulta;
+	}
+
+	public String getCedulaPaciente() {
+		return cedulaPaciente;
+	}
+
+	public void setCedulaPaciente(String cedulaPaciente) {
+		this.cedulaPaciente = cedulaPaciente;
+	}
+
+	public String getMedico() {
+		return medico;
+	}
+
+	public void setMedico(String medico) {
+		this.medico = medico;
+	}
+
+	public Date getFechaCita() {
+		return fechaCita;
+	}
+
+	public void setFechaCita(Date fechaCita) {
+		this.fechaCita = fechaCita;
+	}
+
+	public String getMotivoConsulta() {
+		return motivoConsulta;
+	}
+
+	public void setMotivoConsulta(String motivoConsulta) {
+		this.motivoConsulta = motivoConsulta;
+	}
+
+	public String getProcedimientoConsulta() {
+		return procedimientoConsulta;
+	}
+
+	public void setProcedimientoConsulta(String procedimientoConsulta) {
+		this.procedimientoConsulta = procedimientoConsulta;
+	}
+
+	public boolean isUnidad() {
+		return unidad;
+	}
+
+	public void setUnidad(boolean unidad) {
+		this.unidad = unidad;
+	}
+
+	public String getEstadoConsulta() {
+		return estadoConsulta;
+	}
+
+	public void setEstadoConsulta(String estadoConsulta) {
+		this.estadoConsulta = estadoConsulta;
+	}
+	
+}
