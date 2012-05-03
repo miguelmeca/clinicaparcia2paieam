a65605663513bad331a9b3e2cb97b28f0f854ec0
neq.hbo@gmail.com <neq.hbo@gmail.com@ed173650-50d6-6ff3-37b3-900c6ac7dc41>
2012-05-03 04:39:26 +0000

    bean nuevo AsignarCitasPacienteBean

---
+++ trunk/clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/paciente/AsignarCitasPacienteBean.java
@@ -0,0 +1,103 @@
+package edu.eam.clinica.web.bean.paciente;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.servlet.http.HttpSession;
+
+
+import edu.eam.clinica.jpa.entidades.Consulta;
+import edu.eam.clinica.jpa.entidades.Medico;
+import edu.eam.clinica.jpa.entidades.Paciente;
+import edu.eam.clinica.jpa.entidades.Persona;
+import edu.eam.clinica.jpa.enumeraciones.EstadoConsultaEnum;
+import edu.eam.clinica.jpa.utilidades.FactoryEntityManager;
+
+public class AsignarCitasPacienteBean {
+	
+			private EntityManager em;
+			
+			private Paciente paciente;
+			
+			private Consulta cita;
+
+			private List<Medico> medicos;
+			public AsignarCitasPacienteBean() {
+				
+				em=FactoryEntityManager.getEm();
+				
+			HttpSession sesion=(HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(false);
+			Persona persona= (Persona)sesion.getAttribute("persona");
+			paciente = em.find(Paciente.class, persona.getDocumento());
+			
+			
+			Query query = em.createNamedQuery(Medico.FIND_ALL);
+			medicos=query.getResultList();
+			
+			}
+			
+		
+			public String asignarCita(){
+				
+			List<Consulta> consultas=paciente.getConsultas();	
+			
+			Consulta consulta=new Consulta();
+			consulta.setEstado(EstadoConsultaEnum.EN_ESPERA);
+			consulta.setFechaHora(null);
+			consulta.setMedico(null);
+			consulta.setPaciente(paciente);
+			em.persist(consulta);
+				consultas.add(consulta);
+				paciente.setConsultas(consultas);
+				em.persist(paciente);	
+				return null;
+			}
+		
+			
+		
+
+			public List<Medico> getMedicos() {
+				return medicos;
+			}
+
+
+			public void setMedicos(List<Medico> medicos) {
+				this.medicos = medicos;
+			}
+
+
+			public EntityManager getEm() {
+				return em;
+			}
+
+			public void setEm(EntityManager em) {
+				this.em = em;
+			}
+
+
+			public Paciente getPaciente() {
+				return paciente;
+			}
+
+
+			public void setPaciente(Paciente paciente) {
+				this.paciente = paciente;
+			}
+
+
+			public Consulta getCita() {
+				return cita;
+			}
+
+
+			public void setCita(Consulta cita) {
+				this.cita = cita;
+			}
+
+		
+		
+
+
+}
