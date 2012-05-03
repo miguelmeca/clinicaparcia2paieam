b4358a1e87dd2f698bf8c84024c8398d3745c58a
davidagb20 <davidagb20@gmail.com>
2012-05-03 00:41:17 +0000

    metodo crearCita en el bean AsignarCitaBean :)

--- clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/AsignarCitasBean.java
+++ clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/AsignarCitasBean.java
@@ -4,34 +4,65 @@ import java.sql.Date;
 import java.util.*;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.swing.JOptionPane;
 
+import edu.eam.clinica.jpa.entidades.Consulta;
 import edu.eam.clinica.jpa.entidades.Medico;
+import edu.eam.clinica.jpa.entidades.Paciente;
+import edu.eam.clinica.jpa.enumeraciones.MotivoConsultaEnum;
+import edu.eam.clinica.jpa.enumeraciones.TipoProcedimietoEnum;
 import edu.eam.clinica.jpa.utilidades.FactoryEntityManager;
 
 public class AsignarCitasBean {
 
-	private int nroConsulta;
+	private long nroConsulta;
 	private String cedulaPaciente;
-	private String medico;
+	private String docmedico;
 	private Date fechaCita;
-	private String motivoConsulta;
-	private String procedimientoConsulta;
+	private MotivoConsultaEnum motivoConsulta;
+	private TipoProcedimietoEnum procedimientoConsulta;
 	private boolean unidad;
 	private String estadoConsulta;
-	
+	private String horaCita;
+
 	private EntityManager em;
-	
-	public AsignarCitasBean(){
+
+	public AsignarCitasBean() {
 		em = FactoryEntityManager.getEm();
 	}
-	
-	public String crearCrita(){
-		
-		return null;
-	}
 
-	public int getNroConsulta() {
-		return nroConsulta;
+	public String crearCrita() {
+		
+		Paciente paciente = em.find(Paciente.class, cedulaPaciente);
+		if (paciente != null) {
+			Medico medico = em.find(Medico.class, docmedico);
+			if (medico != null) {
+
+				Query query = em.createNamedQuery(Consulta.FIND_CONSULTA_BY_REGISTRO_MEDICO);
+				query.setParameter(Consulta.PARAMETRO_REGISTRO_MEDICO,docmedico);
+
+				List<Consulta> consultasMedico = query.getResultList();
+
+				for (Consulta consulta : consultasMedico) {
+
+					if (consulta.getFechaHora().equals(fechaCita + "" + horaCita)) {
+
+						JOptionPane.showMessageDialog(null,"la cita ya fue asignada");
+						/*
+						 * lanzar mensaje indicando que la cita ya fue asignada
+						 * en ese dia en esa hora
+						 */
+					}
+				}
+				Consulta consulta = new Consulta(nroConsulta, fechaCita,paciente, medico, motivoConsulta, procedimientoConsulta);
+				em.getTransaction().begin();
+				em.persist(consulta);
+				em.getTransaction().commit();
+				
+			}
+		}
+		return null;
 	}
 
 	public void setNroConsulta(int nroConsulta) {
@@ -46,14 +77,6 @@ public class AsignarCitasBean {
 		this.cedulaPaciente = cedulaPaciente;
 	}
 
-	public String getMedico() {
-		return medico;
-	}
-
-	public void setMedico(String medico) {
-		this.medico = medico;
-	}
-
 	public Date getFechaCita() {
 		return fechaCita;
 	}
@@ -62,19 +85,28 @@ public class AsignarCitasBean {
 		this.fechaCita = fechaCita;
 	}
 
-	public String getMotivoConsulta() {
+	public long getNroConsulta() {
+		return nroConsulta;
+	}
+
+	public void setNroConsulta(long nroConsulta) {
+		this.nroConsulta = nroConsulta;
+	}
+
+	public MotivoConsultaEnum getMotivoConsulta() {
 		return motivoConsulta;
 	}
 
-	public void setMotivoConsulta(String motivoConsulta) {
+	public void setMotivoConsulta(MotivoConsultaEnum motivoConsulta) {
 		this.motivoConsulta = motivoConsulta;
 	}
 
-	public String getProcedimientoConsulta() {
+	public TipoProcedimietoEnum getProcedimientoConsulta() {
 		return procedimientoConsulta;
 	}
 
-	public void setProcedimientoConsulta(String procedimientoConsulta) {
+	public void setProcedimientoConsulta(
+			TipoProcedimietoEnum procedimientoConsulta) {
 		this.procedimientoConsulta = procedimientoConsulta;
 	}
 
@@ -93,5 +125,21 @@ public class AsignarCitasBean {
 	public void setEstadoConsulta(String estadoConsulta) {
 		this.estadoConsulta = estadoConsulta;
 	}
-	
+
+	public String getDocmedico() {
+		return docmedico;
+	}
+
+	public void setDocmedico(String docmedico) {
+		this.docmedico = docmedico;
+	}
+
+	public String getHoraCita() {
+		return horaCita;
+	}
+
+	public void setHoraCita(String horaCita) {
+		this.horaCita = horaCita;
+	}
+
 }
