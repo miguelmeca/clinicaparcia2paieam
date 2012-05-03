d93df58fbed9b23dcbeac30854b397f180827475
apoloeam@gmail.com <apoloeam@gmail.com@ed173650-50d6-6ff3-37b3-900c6ac7dc41>
2012-05-03 00:38:21 +0000

    filtro

--- trunk/clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/InventarioBean.java
+++ trunk/clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/InventarioBean.java
@@ -1,9 +1,12 @@
 package edu.eam.clinica.web.bean.funcionario;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
 
+import edu.eam.clinica.jpa.entidades.Articulo;
 import edu.eam.clinica.jpa.entidades.Funcionario;
 import edu.eam.clinica.jpa.entidades.Inventario;
 import edu.eam.clinica.jpa.entidades.Medicamento;
@@ -14,51 +17,125 @@ import edu.eam.clinica.web.autorizacion.SesionFactory;
 
 public class InventarioBean  {
 
-	
-	private GrupoTerapeuticoEnum grupoTerapeutico;
-	private String registroInvima;
-	private PresentacionEnum presentacion;
+/**
+ * parametros del InvetarioBean
+ */
 	private EntityManager em;
 	private Funcionario funcionario;
+	private String codigoArticulo;
+	private String codigoBarras;
+	private Date fechaIngreso;
+	private Articulo articulo;
+	private String nombre;
 	
 	
+	
+	/**
+	 * Constructor InventarioBean
+	 */
 	public InventarioBean(){
 		
 		em=FactoryEntityManager.getEm();
 		funcionario=(Funcionario)SesionFactory.getSesion();
 	}
 	
-	public List<Medicamento> getMedicamentos(){
+	/**
+	 * metodo que lista los medicamentos utulizando una query que proyecta nombre precio y un count que cuenta por nombres de un grupo por nombre
+	 * @return
+	 */
+	public List<Articulo> getMedicamentos(){
 		
-		return em.createNamedQuery("MedicamentosAll").getResultList();
+		return em.createNamedQuery("MedicamentoAll").getResultList();
 		
+	}
+	/**
+	 * metodo para crear el medicamento en el inventario
+	 * utilizo el codigoarticulo que me digita el usuario y busco un articulo con el y si encuentra algo procedo 
+	 * a buscar el codigo de barra de inventario y si no encutro nada lo creo
+	 */
+	public void crearInventario(){
+	
+		Articulo a=em.find(Articulo.class, codigoArticulo);
+		
+		if(a!=null){
+			
+			Inventario i=em.find(Inventario.class,codigoBarras);
+			if(i==null){
+				
+				em.getTransaction().begin();
+				Inventario inven= new Inventario(a, fechaIngreso, null, codigoBarras, null, funcionario,null);
+				em.persist(inven);
+				em.flush();
+				em.getTransaction().commit();
+			}
+			
+			
+		}
+		
+		
+		
+	}
+
+	public List<Inventario>getfiltrando(){
+		
+		
+		return em.createNamedQuery("filtradoAll").getResultList();
+		
+		
+		
+	}
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public String getCodigoArticulo() {
+		return codigoArticulo;
+	}
+
+	public void setCodigoArticulo(String codigoArticulo) {
+		this.codigoArticulo = codigoArticulo;
+	}
+
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+
+	public Date getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(Date fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	
+
+	public Articulo getArticulo() {
+		return articulo;
+	}
+
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 	
 	
 
-	public GrupoTerapeuticoEnum getGrupoTerapeutico() {
-		return grupoTerapeutico;
-	}
-
-	public void setGrupoTerapeutico(GrupoTerapeuticoEnum grupoTerapeutico) {
-		this.grupoTerapeutico = grupoTerapeutico;
-	}
-
-	public String getRegistroInvima() {
-		return registroInvima;
-	}
-
-	public void setRegistroInvima(String registroInvima) {
-		this.registroInvima = registroInvima;
-	}
-
-	public PresentacionEnum getPresentacion() {
-		return presentacion;
-	}
-
-	public void setPresentacion(PresentacionEnum presentacion) {
-		this.presentacion = presentacion;
-	}
 	
 	
 	
