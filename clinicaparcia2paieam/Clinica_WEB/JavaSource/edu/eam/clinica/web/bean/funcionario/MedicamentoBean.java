d61a9a98728a8e5142a88098adfb46ec90900e5e
davidagb20 <davidagb20@gmail.com>
2012-05-03 03:02:18 +0000

    MedicamentoBean = contiene los atributos a mostrar del medicamento

---
+++ clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/MedicamentoBean.java
@@ -0,0 +1,69 @@
+package edu.eam.clinica.web.bean.funcionario;
+
+public class MedicamentoBean {
+	
+	
+	private String codigoBarras;
+	private long id;
+	private String codigo;
+	private String nombre;
+	private double precio;
+    private int cantidad;
+	
+	public MedicamentoBean(){
+		
+	}
+
+
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+
+
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
+
+}
