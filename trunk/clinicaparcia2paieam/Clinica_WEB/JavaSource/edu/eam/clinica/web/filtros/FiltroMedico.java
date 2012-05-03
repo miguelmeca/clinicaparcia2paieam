19245d50e6554b4a639a36d61b5fc490e74bb5cd
ceaucarpa88@gmail.com <ceaucarpa88@gmail.com@ed173650-50d6-6ff3-37b3-900c6ac7dc41>
2012-05-03 03:01:40 +0000

    filtro de las paginas que corresponden a un Medico    ceaucarpa

--- trunk/clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/filtros/FiltroMedico.java
+++ trunk/clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/filtros/FiltroMedico.java
@@ -1,22 +1,30 @@
 package edu.eam.clinica.web.filtros;
 
 import java.io.IOException;
+
+import javax.faces.context.FacesContext;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import edu.eam.clinica.jpa.entidades.Medico;
+import edu.eam.clinica.jpa.entidades.Persona;
+import edu.eam.clinica.web.autorizacion.SesionFactory;
 
 /**
  * Servlet Filter implementation class Filtro_Medico
  */
-public class Filtro_Medico implements Filter {
+public class FiltroMedico implements Filter {
 
     /**
      * Default constructor. 
      */
-    public Filtro_Medico() {
+    public FiltroMedico() {
         // TODO Auto-generated constructor stub
     }
 
@@ -31,10 +39,22 @@ public class Filtro_Medico implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
-
-		// pass the request along the filter chain
+		//Se obtiene el request de tipo http.
+		HttpServletRequest req=(HttpServletRequest) request;
+		
+		//se obtiene la persona de sesion.
+		Medico user=(Medico) SesionFactory.getValor("medico");
+		//esta en sesion, sigue la peticion.
+		if(user!=null){
+			
+			/*se envia al HttpSession la variable de sesion de tipo medico*/
+			SesionFactory.agregarASesion("medico", user);
+			
+			chain.doFilter(request, response);
+		}else{
+			//se envia a inicio.
+			req.getRequestDispatcher("/pages/inicio.jsf").forward(request, response);
+		}
 		chain.doFilter(request, response);
 	}
 
