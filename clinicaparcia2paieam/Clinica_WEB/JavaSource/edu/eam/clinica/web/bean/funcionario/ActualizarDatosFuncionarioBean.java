13718c34b3d0d173820dc215268ade4394ab9e9f
juan.lnd26 <juan.lnd26@gmail.com>
2012-05-03 01:33:13 +0000

    ActualizarDatosFuncionarioBean - Modificacion

--- clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/ActualizarDatosFuncionarioBean.java
+++ clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/ActualizarDatosFuncionarioBean.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import edu.eam.clinica.jpa.entidades.Ciudad;
 import edu.eam.clinica.jpa.entidades.Funcionario;
 import edu.eam.clinica.jpa.entidades.Telefono;
+import edu.eam.clinica.jpa.enumeraciones.SexoEnum;
+import edu.eam.clinica.jpa.enumeraciones.TipoDocumentoEnum;
 import edu.eam.clinica.jpa.utilidades.FactoryEntityManager;
 import edu.eam.clinica.web.autorizacion.SesionFactory;
+import edu.eam.clinica.web.filtros.FiltroFuncionario;
 
 public class ActualizarDatosFuncionarioBean {
 
@@ -15,32 +19,119 @@ public class ActualizarDatosFuncionarioBean {
 	 * funcionaro logueado
 	 */
 	private Funcionario funcionario;
-
-	private String documento="meme";
+	/*
+	 * documento del funcionario
+	 */
+	private String documento;
+	/*
+	 * el tipo de documento del funcionario
+	 */
+	private TipoDocumentoEnum tipoDocumento;
+	/*
+	 * primer nombre del funcionario
+	 */
+	private String primerNombre;
+	/*
+	 * segundo nombre del funcionario
+	 */
+	private String segundoNombre;
+	/*
+	 * primer apellido del funcionario
+	 */
+	private String primerApellido;
+	/*
+	 * segundo apellido del funcionario
+	 */
+	private String segundoApellido;
+	/*
+	 * direccion del funcionario
+	 */
+	private String direccion;
+	/*
+	 * ciudad del funcionario
+	 */
+	private Ciudad ciudad;
+	/*
+	 * sexo del funcionario (masculino) o (femenino)
+	 */
+	private SexoEnum sexo;
+	/*
+	 * correo electronico (E-Mail) del funcionario
+	 */ 
+	private String eMail;
+	/*
+	 * telefonos del funcionario
+	 */
+	private List<Telefono> telefonos;
+	/*
+	 * nombre de usuario del funcionario
+	 */
+	private String logIn;
+	/*
+	 * contraseña de acceso del funcionario
+	 */
+	private String pass;
+	
+	
 	/*
 	 * EntityManager para hacer uso de la persistencia
 	 */
 	private EntityManager em;
 
+	/*
+	 * constructor
+	 */
 	public ActualizarDatosFuncionarioBean() {
-
 		em = FactoryEntityManager.getEm();
-		//funcionario = (Funcionario) SesionFactory.getSesion();
-		//funcionario=em.find(Funcionario.class, 1234);
-		//documento=funcionario.getDocumento();
+		funcionario = (Funcionario) SesionFactory.getSesion();
 	}
 
+	/*
+	 * metodo con el cual se actualizaran los datos personales de un funcionario
+	 */
+	public void actualizarDatosPersonalesFuncionario(){
+		
+	
+			em.getTransaction().begin();
+			
+			funcionario.setCiudad(ciudad);
+			funcionario.setDireccion(direccion);
+			funcionario.setDocumento(documento);
+			funcionario.setEmail(eMail);
+			funcionario.setPrimerApellido(primerApellido);
+			funcionario.setPrimerNombre(primerNombre);
+			funcionario.setSegundoApellido(segundoApellido);
+			funcionario.setSegundoNombre(segundoNombre);
+			funcionario.setSexo(sexo);
+			funcionario.setTipoDocumento(tipoDocumento);
+			
+			em.refresh(funcionario);
+			em.flush();
+			
+			em.getTransaction().commit();
+		
+		
+		
+		
+	}
+	
 	public List<Telefono> getTelefonosFuncionario() {
-		if (funcionario != null) {
-			return funcionario.getTelefonos();
-		} else {
-			/*
-			 * lanzar mensaje global indicando que no hay un funcionario en la
-			 * sesion
-			 */
-			return null;
-		}
+		
+		return em.createNamedQuery(funcionario.FIND_ALL).getResultList();
 
+	}
+	
+	public void actualizarDatosUsuarioFuncionario(){
+		em.getTransaction().begin();
+		
+		funcionario.setLogin(logIn);
+		funcionario.setPassword(pass);
+		
+		em.refresh(funcionario);
+		em.flush();
+		
+		em.getTransaction().commit();
+		
 	}
 
 	public Funcionario getFuncionario() {
@@ -58,6 +149,88 @@ public class ActualizarDatosFuncionarioBean {
 	public void setDocumento(String documento) {
 		this.documento = documento;
 	}
+
+	public TipoDocumentoEnum getTipoDocumento() {
+		return tipoDocumento;
+	}
+
+	public void setTipoDocumento(TipoDocumentoEnum tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+
+	public String getPrimerNombre() {
+		return primerNombre;
+	}
+
+	public void setPrimerNombre(String primerNombre) {
+		this.primerNombre = primerNombre;
+	}
+
+	public String getSegundoNombre() {
+		return segundoNombre;
+	}
+
+	public void setSegundoNombre(String segundoNombre) {
+		this.segundoNombre = segundoNombre;
+	}
+
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public SexoEnum getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(SexoEnum sexo) {
+		this.sexo = sexo;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public List<Telefono> getTelefonos() {
+		return telefonos;
+	}
+
+	public void setTelefonos(List<Telefono> telefonos) {
+		this.telefonos = telefonos;
+	}
+
+	
 	
 	
 
