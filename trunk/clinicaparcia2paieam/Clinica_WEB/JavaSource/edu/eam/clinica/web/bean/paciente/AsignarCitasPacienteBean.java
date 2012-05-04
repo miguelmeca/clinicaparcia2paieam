c209f36c7458b8335946f169d74a55bcb7700e84
neq.hbo@gmail.com <neq.hbo@gmail.com@ed173650-50d6-6ff3-37b3-900c6ac7dc41>
2012-05-04 17:02:19 +0000

    mejor al metodo asignarCitas el cual ya asigna una fecha a la cita correctamente. correjido combo medicos el cual resive una lista de selectitems. creado variables medicos,fecha,hora.

--- trunk/clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/paciente/AsignarCitasPacienteBean.java
+++ trunk/clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/paciente/AsignarCitasPacienteBean.java
@@ -1,11 +1,20 @@
 package edu.eam.clinica.web.bean.paciente;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.servlet.http.HttpSession;
+
+import org.richfaces.iterator.ForEachIterator;
+
+import com.sun.org.apache.xerces.internal.impl.dv.xs.YearDV;
 
 import edu.eam.clinica.jpa.entidades.Consulta;
 import edu.eam.clinica.jpa.entidades.Medico;
@@ -21,9 +30,14 @@ public class AsignarCitasPacienteBean {
 	private Paciente paciente;
 
 	private Consulta cita;
+	
+	private Calendar fecha;
+	private SelectItem horaCita;
+	
+	private List<SelectItem> medicos;
+	private SelectItem medicoAsignado;
 
-	private List<Medico> medicos;
-
+	
 	public AsignarCitasPacienteBean() {
 
 		em = FactoryEntityManager.getEm();
@@ -33,10 +47,11 @@ public class AsignarCitasPacienteBean {
 		Persona persona = (Persona) sesion.getAttribute("persona");
 		paciente = em.find(Paciente.class, persona.getDocumento());
 
-		Query query = em.createNamedQuery(Medico.FIND_ALL);
-		medicos = query.getResultList();
+	
 
 	}
+	
+	
 
 	public String asignarCita() {
 
@@ -44,7 +59,13 @@ public class AsignarCitasPacienteBean {
 
 		Consulta consulta = new Consulta();
 		consulta.setEstado(EstadoConsultaEnum.EN_ESPERA);
-		consulta.setFechaHora(null);//fecha del rich:calendar.
+		String hora=horaCita.getValue()+"";
+		
+		String [] horaYMin=hora.split(":");
+		
+		fecha.add(Calendar.HOUR_OF_DAY,Integer.parseInt(horaYMin[0]));
+		fecha.add(Calendar.MINUTE,Integer.parseInt(horaYMin[1]));
+		consulta.setFechaHora(fecha.getTime());//fecha del rich:calendar.
 		consulta.setMedico(null);	//medico q se elijio.disponibles.	
 		consulta.setPaciente(paciente);
 		em.persist(consulta);
@@ -54,11 +75,28 @@ public class AsignarCitasPacienteBean {
 		return null;
 	}
 
-	public List<Medico> getMedicos() {
+	public List<SelectItem> getMedicos() {
+		Query query = em.createNamedQuery(Medico.FIND_ALL);
+	List<Medico> medico = query.getResultList();
+		for (Iterator iterator = medico.iterator(); iterator.hasNext();) {
+			Medico medico2 = (Medico) iterator.next();
+			
+			Query query2=em.createNamedQuery(Consulta.FIND_CONSULTA_BY_MEDICO_AND_FECHAS);
+			query2.setParameter(Consulta.PARAMETRO_REGISTRO_MEDICO, medico2.getRegistroMedico());
+			query2.setParameter(Consulta.PARAMETRO_MENOR_FECHA, fecha.getTime());
+			query2.setParameter(Consulta.PARAMETRO_MAYOR_FECHA, fecha.getTime());
+			List<Consulta>consultasMedico=query2.getResultList();
+			Consulta consulta=consultasMedico.get(0);
+			if(consulta!=null){
+			
+			SelectItem itemMedico=new SelectItem(medico2.getDocumento(),medico2.getNombre() );
+			medicos.add(itemMedico);
+			}
+		}
 		return medicos;
 	}
 
-	public void setMedicos(List<Medico> medicos) {
+	public void setMedicos(List<SelectItem> medicos) {
 		this.medicos = medicos;
 	}
 
@@ -73,6 +111,20 @@ public class AsignarCitasPacienteBean {
 	public Paciente getPaciente() {
 		return paciente;
 	}
+	
+	
+
+	public Calendar getFecha() {
+		return fecha;
+	}
+
+
+
+	public void setFecha(Calendar fecha) {
+		this.fecha = fecha;
+	}
+
+
 
 	public void setPaciente(Paciente paciente) {
 		this.paciente = paciente;
@@ -81,6 +133,19 @@ public class AsignarCitasPacienteBean {
 	public Consulta getCita() {
 		return cita;
 	}
+	
+
+	public SelectItem getHoraCita() {
+		return horaCita;
+	}
+
+
+
+	public void setHoraCita(SelectItem horaCita) {
+		this.horaCita = horaCita;
+	}
+
+
 
 	public void setCita(Consulta cita) {
 		this.cita = cita;
