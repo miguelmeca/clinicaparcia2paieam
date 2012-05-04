46c8c2a10a9a63921d7ecea3da56db668e607458
apoloeam <apoloeam@gmail.com>
2012-05-04 00:19:48 +0000

    consultas y listados  como son

--- clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/InventarioBean.java
+++ clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/InventarioBean.java
@@ -1,5 +1,6 @@
 package edu.eam.clinica.web.bean.funcionario;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -7,10 +8,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.NamedQuery;
 import javax.persistence.Query;
 
+import org.hibernate.validator.Pattern;
+
 import edu.eam.clinica.jpa.entidades.Articulo;
 import edu.eam.clinica.jpa.entidades.Consulta;
 import edu.eam.clinica.jpa.entidades.Funcionario;
 import edu.eam.clinica.jpa.entidades.Inventario;
+import edu.eam.clinica.jpa.entidades.ListarMedicamentos;
 import edu.eam.clinica.jpa.entidades.Medicamento;
 import edu.eam.clinica.jpa.enumeraciones.GrupoTerapeuticoEnum;
 import edu.eam.clinica.jpa.enumeraciones.PresentacionEnum;
@@ -26,9 +30,15 @@ public class InventarioBean  {
 	private Funcionario funcionario;
 	private String codigoArticulo;
 	private String codigoBarras;
-	private Date fechaIngreso;
+	private Date fechaIngreso= new Date();
 	private Articulo articulo;
+	
+	@Pattern(regex = "[a-zA-Z]*", message = "solo letras.")
 	private String nombre;
+	private List<Articulo>articulos;
+	private ArrayList<ListarMedicamentos>listaMedicamentos;
+	private List<Inventario>filtrados;
+	
 	
 	
 	
@@ -38,21 +48,35 @@ public class InventarioBean  {
 	public InventarioBean(){
 		
 		em=FactoryEntityManager.getEm();
-		//sacar el funcionario.
 		funcionario=(Funcionario)SesionFactory.getSesion();
+		listaMedicamentos= new ArrayList<ListarMedicamentos>();
 	}
 	
 	/**
-	 * metodo que lista los medicamentos utulizando una query que proyecta nombre precio y un count que cuenta por nombres de un grupo por nombre
+	 * metodo que lista los medicamentos utulizando  2 consultas la primera saca todos los articulos a esta le saco un codigo y con dicho codigo hago
+	 * otra consulta que me cuenta cuantas cantidades tengo de ese articulo o medicamento
 	 * @return
 	 */
-//	public List<Articulo> getMedicamentos(){
-//		
-//		Query query= em.createNamedQuery(Inventario.CONSULTA_FIND_BY_MEDICAMENTOS);
-//		List<Articulo>medicamentos= query.getResultList();
-//		return medicamentos;
-//		
-//	}
+	public List<ListarMedicamentos> getListaMedicamentos(){
+		
+		Query query= em.createNamedQuery(Articulo.FIND_ALL);
+		articulos= query.getResultList();
+		
+		for(int i=0;i<articulos.size();i++){
+			
+			long codigo =articulos.get(i).getId();
+			String nombre= articulos.get(i).getNombre();
+			
+			Query query2= em.createNamedQuery(Inventario.FIND_ARTICULO_BY_CANTIDAD);
+			query2.setParameter(Inventario.PARAMETRO_CODIGO, codigo);
+			
+			int cantidad= (Integer) query2.getSingleResult();
+			ListarMedicamentos medicamento=new ListarMedicamentos(nombre, codigo, cantidad);
+			listaMedicamentos.add(medicamento);
+		}
+		return listaMedicamentos;
+		
+	}
 	/**
 	 * metodo para crear el medicamento en el inventario
 	 * utilizo el codigoarticulo que me digita el usuario y busco un articulo con el y si encuentra algo procedo 
@@ -66,37 +90,39 @@ public class InventarioBean  {
 			
 			Inventario i=em.find(Inventario.class,codigoBarras);
 			if(i==null){
-				
-//				em.getTransaction().begin();
-//				Inventario inven= new Inventario(a, fechaIngreso, null, codigoBarras, null, funcionario,null);
-//				em.persist(inven);
-//				em.flush();
-//				em.getTransaction().commit();
-			}
-			
-			
-		}
-		
-		
-		
+				Inventario inven= new Inventario(a, fechaIngreso, null, codigoBarras, null, funcionario,null);
+				em.getTransaction().begin();
+				em.persist(inven);
+				em.getTransaction().commit();
+			}	
+		}	
 	}
 
 	/**
 	 * metodo que filtra los medicamentos por el nombre entrdas y salidas
 	 * @return
 	 */
-//	public List<Inventario>getfiltrando(){
-//		
-//		Query query= em.createNamedQuery(Inventario.CONSULTA_FIND_BY_NOMBRE);
-//		query.setParameter(Inventario.PARAMENTRO_NOMBRE,nombre);
-//		
-//		List<Inventario>inventario= query.getResultList();
-//		
-//		return inventario;
-//		
-//		
-//		
-//	}
+	public List<Inventario> buscarfiltrado(){
+		
+		Query query= em.createNamedQuery(Articulo.FIND_ARTICULO_BY_NOMBREARTI);
+		query.setParameter(Articulo.PARAMENTRO_NOMBREART,nombre);
+		
+		List<Articulo>articulitos=query.getResultList();
+		
+		for(int i=0;i<articulitos.size();i++){
+			long codigos=articulitos.get(i).getId();
+			Query query2= em.createNamedQuery(Inventario.CONSULTA_FIND_BY_CODIGO);
+			query2.setParameter(Inventario.PARAMETRO_CODIGO, codigos);
+			filtrados= query2.getResultList();
+		}
+		
+		return filtrados;
+	
+		
+		
+	}
+		
+	
 	public Funcionario getFuncionario() {
 		return funcionario;
 	}
@@ -146,6 +172,23 @@ public class InventarioBean  {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	public List<Articulo> getArticulos() {
+		return articulos;
+	}
+
+	public void setArticulos(List<Articulo> articulos) {
+		this.articulos = articulos;
+	}
+
+	public List<Inventario> getFiltrados() {
+		return filtrados;
+	}
+
+	public void setFiltrados(List<Inventario> filtrados) {
+		this.filtrados = filtrados;
+	}
+
 	
 	
 
