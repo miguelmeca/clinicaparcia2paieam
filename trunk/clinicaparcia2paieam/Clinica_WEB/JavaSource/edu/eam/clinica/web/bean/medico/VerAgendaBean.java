49030bd3f5a01f49f1bef84734b685c558673ff7
alejitacuyabra@gmail.com <alejitacuyabra@gmail.com@ed173650-50d6-6ff3-37b3-900c6ac7dc41>
2012-05-05 15:58:21 +0000

    Alejandra Grajales: Actualizacion de el bean de verAgendaCompleto

--- trunk/clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/medico/VerAgendaBean.java
+++ trunk/clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/medico/VerAgendaBean.java
@@ -1,141 +1,159 @@
 package edu.eam.clinica.web.bean.medico;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.management.loading.PrivateClassLoader;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.hibernate.validator.Length;
+import org.hibernate.validator.Pattern;
+
+import edu.eam.clinica.jpa.entidades.Consulta;
+import edu.eam.clinica.jpa.entidades.Medico;
+import edu.eam.clinica.jpa.entidades.Persona;
+import edu.eam.clinica.jpa.utilidades.FactoryEntityManager;
+import edu.eam.clinica.web.autorizacion.SesionFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
-import edu.eam.clinica.jpa.entidades.Consulta;
-import edu.eam.clinica.jpa.entidades.Medico;
-import edu.eam.clinica.jpa.utilidades.FactoryEntityManager;
-import edu.eam.clinica.web.autorizacion.SesionFactory;
-
 public class VerAgendaBean {
-
-	/*
+	/**
 	 * Parametro que permite acceder a los datos de la base de datos
 	 */
 	private EntityManager em;
-	/*
+	/**
 	 * fecha que se selecciona del rich:calendar para ver las consultas de la
 	 * misma
 	 */
 	private Date fecha;
-	/*
-	 * fecha de hoy para ver las consultas del mimo
+	/**
+	 * fecha de hoy para ver las consultas del mismo
 	 */
-	private Date fecha1;
-	/*
-	 * Medico  utilizado para la simulacion de session
+	private Calendar fecha1;
+	/**
+	 * Medico utilizado para la simulacion de session
 	 */
 	private Medico medico;
-	/*
+	/**
 	 * Parametro que permite utilizar la variable en session
 	 */
 	private SesionFactory s;
-	/*
+	/**
 	 * Id de la consulta seleccionada en la tabla
 	 */
 	private long idC;
+	/**
+	 * Consultas que se van a listar en la tabla
+	 */
+	private List<Consulta> consultas;
 
 	public VerAgendaBean() {
-		fecha1 = new Date();
+		fecha1 = Calendar.getInstance();
 		em = FactoryEntityManager.getEm();
-/*
- * Estas dos lineas se usan para la simulacion de la variable medico en session
- */
-//		medico = em.find(Medico.class, "1234");
-//		SesionFactory.agregarASesion("persona", medico);
+		/*
+		 * Estas dos lineas se usan para la simulacion de la variable medico en
+		 * session
+		 */
+		// medico = em.find(Medico.class, "1234");
+		// SesionFactory.agregarASesion("persona", medico);
+		//
 
-		 medico=(Medico) SesionFactory.getValor("persona");
+		medico = (Medico) SesionFactory.getValor("persona");
 
 	}
 
-	/*
-	 * Metodo que me optiene las consultas de un medico en una fecha en
+	/**
+	 * * Metodo que me optiene las consultas de un medico en una fecha en
 	 * particular
-	 */
-	public List<Consulta> getConsultasM() {
-
-		Date fechaM = new Date();
-		fechaM.setYear(fecha.getYear());
-		fechaM.setMonth(fecha.getMonth());
-		fechaM.setDate(fecha.getDate());
-		fechaM.setHours(0);
-		fechaM.setMinutes(00);
-		fecha.setHours(23);
-		fecha.setMinutes(59);
-
-		Query q = em
-				.createNamedQuery(Consulta.FIND_CONSULTA_BY_MEDICO_AND_FECHAS);
-		q.setParameter(Consulta.PARAMETRO_MENOR_FECHA, fechaM);
-		q.setParameter(Consulta.PARAMETRO_MAYOR_FECHA, fecha);
-		q.setParameter(Consulta.PARAMETRO_REGISTRO_MEDICO,
-				medico.getRegistroMedico());
-		return q.getResultList();
-
-	}
-	
-	/*
-	 * Metodo que  optiene las consultas de un medico en el dia actual
 	 * 
+	 * @return null ya que no requiere usar reglas de navegacion
 	 */
-	public List<Consulta> getConsultasD() {
+	public String consultasFecha() {
 
-		Date fechaM = new Date();
-//		fechaM.setYear(fecha1.getYear());
-//		fechaM.setMonth(fecha1.getMonth());
-//		fechaM.setDate(fecha1.getDate());
-//		fechaM.setHours(0);
-//		fechaM.setMinutes(00);
-//		fecha1.setHours(23);
-//		fecha1.setMinutes(59);
-		Calendar c=Calendar.getInstance();
-		//c.set(Calendar.MINUTE, 34);
-		c.getTime();//date del calendar.
+		Calendar fecha0 = Calendar.getInstance();
+		Calendar fecha2 = Calendar.getInstance();
 
+		fecha0.setTime(fecha);
+		fecha0.set(Calendar.HOUR_OF_DAY, 0);
+		fecha0.set(Calendar.MINUTE, 0);
+		fecha0.set(Calendar.SECOND, 0);
+
+		fecha2.setTime(fecha);
+		fecha2.set(Calendar.HOUR_OF_DAY, 23);
+		fecha2.set(Calendar.MINUTE, 59);
+		fecha2.set(Calendar.SECOND, 59);
 
 		Query q = em
 				.createNamedQuery(Consulta.FIND_CONSULTA_BY_MEDICO_AND_FECHAS);
-		q.setParameter(Consulta.PARAMETRO_MENOR_FECHA, fechaM);
-		q.setParameter(Consulta.PARAMETRO_MAYOR_FECHA, fecha1);
+		q.setParameter(Consulta.PARAMETRO_MENOR_FECHA, fecha0.getTime());
+		q.setParameter(Consulta.PARAMETRO_MAYOR_FECHA, fecha2.getTime());
 		q.setParameter(Consulta.PARAMETRO_REGISTRO_MEDICO,
 				medico.getRegistroMedico());
-		return q.getResultList();
+
+		consultas = q.getResultList();
+		return null;
+
 	}
 
-	public EntityManager getEm() {
-		return em;
+	/**
+	 * * Metodo que me optiene las consultas de un medico en el dia de actual
+	 * 
+	 * @return null ya que no requiere usar reglas de navegacion
+	 */
+	public String consultasDia() {
+
+		Calendar fechaM = Calendar.getInstance();
+		fechaM.set(fecha1.get(Calendar.YEAR), fecha1.get(Calendar.MONTH),
+				fecha1.get(Calendar.DATE), 0, 00, 00);
+		fecha1.set(fecha1.get(Calendar.YEAR), fecha1.get(Calendar.MONTH),
+				fecha1.get(Calendar.DATE), 23, 59, 00);
+
+		Query q = em
+				.createNamedQuery(Consulta.FIND_CONSULTA_BY_MEDICO_AND_FECHAS);
+		q.setParameter(Consulta.PARAMETRO_MENOR_FECHA, fechaM.getTime());
+		q.setParameter(Consulta.PARAMETRO_MAYOR_FECHA, fecha1.getTime());
+		q.setParameter(Consulta.PARAMETRO_REGISTRO_MEDICO,
+				medico.getRegistroMedico());
+		System.out.println(fechaM);
+		consultas = q.getResultList();
+
+		return null;
 	}
-	
-	/*
-	 * Metodo que permite  enviar la variable consulta
-	 * en session, y ademas permite navegar a la pagina
-	 * atenderConsulta
+
+	/**
+	 * Metodo que permite enviar la variable consulta en session, y ademas
+	 * permite navegar a la pagina atenderConsulta	  
+	 * @return la regla de navegacion para ir a la pagina de atender las consul
 	 */
 	public String actionConsulta() {
 		Consulta c = em.find(Consulta.class, idC);
-		//crear un hoy y compararalo solo por dia,
-		//if (fecha == c.getFechaHora() || fecha1 == c.getFechaHora()) {
 
+		if (c.getFechaHora().getDay() == fecha1.getTime().getDay()) {
 			s = new SesionFactory();
 			s.agregarASesion("consulta", c);
 
 			return "atenderConsulta";
-//		} else {
-//			FacesContext
-//					.getCurrentInstance()
-//					.addMessage(
-//							null,
-//							new FacesMessage(
-//									FacesMessage.SEVERITY_ERROR,
-//									"Esta consulta no se puede atender debido a que hoy no es el dia indicado",
-//									null));
-//		}
-//		return null;
+		} else {
+			FacesContext
+					.getCurrentInstance()
+					.addMessage(
+							null,
+							new FacesMessage(
+									FacesMessage.SEVERITY_ERROR,
+									"Esta consulta no se puede atender debido a que hoy no es el dia indicado",
+									null));
+		}
+		return null;
 
+	}
+
+	public EntityManager getEm() {
+		return em;
 	}
 
 	public long getIdC() {
@@ -146,16 +164,24 @@ public class VerAgendaBean {
 		this.idC = idC;
 	}
 
-	public void setEm(EntityManager em) {
-		this.em = em;
-	}
-
 	public Date getFecha() {
 		return fecha;
 	}
 
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
+	}
+
+	public void setEm(EntityManager em) {
+		this.em = em;
+	}
+
+	public List<Consulta> getConsultas() {
+		return consultas;
+	}
+
+	public void setConsultas(List<Consulta> consultas) {
+		this.consultas = consultas;
 	}
 
 }
