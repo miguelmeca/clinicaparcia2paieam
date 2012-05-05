50815b1f81b63cd4e6645df198185b7442864d5b
lauramilena93 <lauramilena93@gmail.com>
2012-05-05 03:17:42 +0000

    metodo validarMedicamento * actualizacion metodo buscarFormulaMedica

--- clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/EntregarFormulaMedicaBean.java
+++ clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/EntregarFormulaMedicaBean.java
@@ -1,5 +1,6 @@
 package edu.eam.clinica.web.bean.funcionario;
 
+import java.util.Calendar;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
@@ -13,44 +14,72 @@ import edu.eam.clinica.jpa.entidades.Articulo;
 import edu.eam.clinica.jpa.entidades.DetalleFormulaMedica;
 import edu.eam.clinica.jpa.entidades.FormulaMedica;
 import edu.eam.clinica.jpa.entidades.Funcionario;
+import edu.eam.clinica.jpa.entidades.Inventario;
 import edu.eam.clinica.jpa.utilidades.FactoryEntityManager;
 import edu.eam.clinica.web.autorizacion.SesionFactory;
 
 public class EntregarFormulaMedicaBean {
-	/*
+	/**
 	 * codigo de la formula medica
 	 */
 	@NotNull
 	@Pattern(regex = "[0-9]*",message="El codigo solo contiene numeros")
 	private long codFormula;
 	
-	/*
+	/**
 	 * EntityManager para hacer uso de la persistencia
 	 */
 	private EntityManager em;
 	
-	/*
+	/**
 	 * Persona (Funcionario) que inicio sesion
 	 */
 
 	private Funcionario funcionario;
 	
-	/*
+	/**
 	 * FactorySesion para obtener la sesion que se ha iniciado
 	 */
 	
 	private SesionFactory sf;
 	
+	/**
+	 * List para guardar los DetalleFormulaMedica que arroje la busqueda de la formula de un paciente
+	 */
+	List<DetalleFormulaMedica> detalles;
+	
+	/**
+	 * Codigo de barras del articulo ingresado en el modalPanel
+	 */
+	private String codigoBarras;
+	
+	/**
+	 * List para guardar los medicamentos que se pueden entregar al paciente
+	 */
+	List<Articulo> medicamentosEntrega;
+	
+	/**
+	 * Atributo String para guardar el motivo de la salida del medicamento
+	 */
+	private String motivoSalida;
+	
+	
+	
 	public EntregarFormulaMedicaBean(){
 		em=FactoryEntityManager.getEm();
-		//el funcionario de la session con getValor.
-	    funcionario=(Funcionario) sf.getSesion();	
+	    funcionario=(Funcionario) sf.getValor("persona");	
 	}
 	
+	/**
+	 * metodo para buscar la formula medica ingresada por un funcionario para
+	 *  la entrega de medicamentos de un paciente
+	 * @return regla de navegacion: en esta caso ninguna
+	 */
 	public String buscarFormulaMedica(){
 		
-		if(funcionario!=null){
-		
+		/*
+		 * Buscar la formula medica del paciento con el codigo ingresado por el funcionario
+		 */
 		FormulaMedica formula=em.find(FormulaMedica.class,codFormula);
 		
 		/*
@@ -58,33 +87,102 @@ public class EntregarFormulaMedicaBean {
 		 */
 		if(formula !=null){
 			
-			
-			//cargar los medicamentos que esten en la formula encontrada
-			 
 			/*
-			 * consulta para sacar los medicamentos de la formula medica encontrada
+			 * cargar los DetalleFormulaMedica de la formula buscada
+			 * query para sacar los detalles con sus medicamentos de la formula encontrada
 			 */
 			Query consulta=em.createNamedQuery(DetalleFormulaMedica.FIND_DETALLE_FORMULA_MEDICA_BY_ID_FORMULA_MEDICA);
 			consulta.setParameter(DetalleFormulaMedica.PARAMETRO_ID_FORMULA_MEDICA,codFormula);
 			
 			/*
-			 * los medicamentos quedan guardados en la lista llamada detalles
+			 * los detalles con sus medicamentos quedan guardados en la lista llamada detalles
 			 */
 			
-			List<DetalleFormulaMedica> detalles=consulta.getResultList();
-//			DetalleFormulaMedica detalleForm=new DetalleFormulaMedica();
-//			
-//			for (DetalleFormulaMedica detalleFormulaMedica : detalles) {
-//				
-//				Articulo articulo=detalleFormulaMedica.getArticulo();
-//				detalleForm.setArticulo(articulo);
-//			}
+			detalles=consulta.getResultList();
 			
 		}else{
 			
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "La Formula No pudo se encontrada", null));
 		}
+		
+		return null;
+	}
+	
+	/**
+	 * metodo para verificar si el medicamento tiene stock en el inventario de la clinica
+	 * @return
+	 */
+	public String validarMedicamento(){
+		
+		/*
+		 * query para buscar por codigo de barras el articulo en el inventario que no tenga
+		 * salidaInventario
+		 */
+	     Query query=em.createNamedQuery(Inventario.CONSULTA_FIND_BY_ARTICULO);
+	     query.setParameter(Inventario.PARAMETRO_CODIGO, codigoBarras);
+	     
+	     /*
+	      * lista donde se guardan los inventarios encontrados 
+	      */
+	     List<Inventario> articuloInventario=query.getResultList();
+	     
+	     /*
+	      * variable para guardar la cantidad que se debe entregar del medicamento
+	      */
+	     int cantidadArticulo=0;
+	     
+	     
+	     for (DetalleFormulaMedica detalle : detalles) {
+			
+	    	 if(codigoBarras.equals(detalle.getArticulo().getCodigo())){
+	    		 cantidadArticulo=detalle.getCantidad();
+	    	 }
 		}
+	     
+	     /*
+	      * validar si el articulo en el inventario fue encontrado
+	      */
+	     
+	     if(articuloInventario.size()>0){
+	    
+	    	 /*
+	    	  * preguntar si la cantidad es menor o igual a la cantidad que hay en el inventario
+	    	  */
+	    	 if(cantidadArticulo <= articuloInventario.size()){
+	    		 
+	    		 /*
+	    		  * crear salidas del inventario para la cantidad de medicamentos que se van
+	    		  * a entregar al paciente
+	    		  */
+	    		 for (int i=0; i<= cantidadArticulo;i++) {
+	    			 
+	    			 /*
+	    			  * la fecha de actual para modificar la fecha de salida del inventario
+	    			  */
+	    			 Calendar hoy=Calendar.getInstance();
+	    			 /*
+	    			  * sacar el inventario de la lista de inventarios encontrada anteriormente
+	    			  */
+	    			 Inventario medicamentoAEntregar=articuloInventario.get(i);
+	    			 
+	    			 /*
+	    			  * cambiar la hora de la salida 
+	    			  */
+	    			 medicamentoAEntregar.setFechaSalida(hoy.getTime());
+	    			 /*
+	    			  * guardar el funcionario que va a entregar el medicamento
+	    			  */
+	    			 medicamentoAEntregar.setFuncionarioSalida(funcionario);
+	    			 /*
+	    			  * guardar el motivo por el cual se da salida a ese medicamento
+	    			  */
+	    			 medicamentoAEntregar.setMotivoSalida(motivoSalida);
+	    			 
+	    			 medicamentosEntrega.add(medicamentoAEntregar.getArticulo());
+	    			 
+				}
+	    	 }
+	     }
 		return null;
 	}
 
@@ -96,6 +194,14 @@ public class EntregarFormulaMedicaBean {
 		this.codFormula = codFormula;
 	}
 
+	public EntityManager getEm() {
+		return em;
+	}
+
+	public void setEm(EntityManager em) {
+		this.em = em;
+	}
+
 	public Funcionario getFuncionario() {
 		return funcionario;
 	}
@@ -103,6 +209,47 @@ public class EntregarFormulaMedicaBean {
 	public void setFuncionario(Funcionario funcionario) {
 		this.funcionario = funcionario;
 	}
+
+	public SesionFactory getSf() {
+		return sf;
+	}
+
+	public void setSf(SesionFactory sf) {
+		this.sf = sf;
+	}
+
+	public List<DetalleFormulaMedica> getDetalles() {
+		return detalles;
+	}
+
+	public void setDetalles(List<DetalleFormulaMedica> detalles) {
+		this.detalles = detalles;
+	}
+
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+
+	public List<Articulo> getMedicamentosEntrega() {
+		return medicamentosEntrega;
+	}
+
+	public void setMedicamentosEntrega(List<Articulo> medicamentosEntrega) {
+		this.medicamentosEntrega = medicamentosEntrega;
+	}
+
+	public String getMotivoSalida() {
+		return motivoSalida;
+	}
+
+	public void setMotivoSalida(String motivoSalida) {
+		this.motivoSalida = motivoSalida;
+	}
+
 	
 	
 }
