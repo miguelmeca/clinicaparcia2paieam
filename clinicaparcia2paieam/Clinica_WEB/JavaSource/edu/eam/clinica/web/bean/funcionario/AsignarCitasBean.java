79b2a8f7cf9eb47757f652eeadaaa99990b955f2
davidagb20 <davidagb20@gmail.com>
2012-05-06 04:35:02 +0000

    Actualizacion de metodos. nuevos metodo agregados, listar enumeraciones, listar horarios de medicos, listar citas de los pacientes.

--- clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/AsignarCitasBean.java
+++ clinicaparcia2paieam/Clinica_WEB/JavaSource/edu/eam/clinica/web/bean/funcionario/AsignarCitasBean.java
@@ -1,6 +1,5 @@
 package edu.eam.clinica.web.bean.funcionario;
 
-import java.sql.Date;
 import java.util.*;
 
 import javax.faces.application.FacesMessage;
@@ -13,21 +12,64 @@ import javax.swing.JOptionPane;
 import edu.eam.clinica.jpa.entidades.Consulta;
 import edu.eam.clinica.jpa.entidades.Medico;
 import edu.eam.clinica.jpa.entidades.Paciente;
+import edu.eam.clinica.jpa.entidades.Persona;
 import edu.eam.clinica.jpa.enumeraciones.MotivoConsultaEnum;
+import edu.eam.clinica.jpa.enumeraciones.SexoEnum;
+import edu.eam.clinica.jpa.enumeraciones.TipoDocumentoEnum;
 import edu.eam.clinica.jpa.enumeraciones.TipoProcedimietoEnum;
 import edu.eam.clinica.jpa.utilidades.FactoryEntityManager;
 
 public class AsignarCitasBean {
 
+	/**
+	 * Guarda la fecha que se desea buscar
+	 */
+	private Date fechaBuscar;
+	
+	/**
+	 * contiene la cedula del paciente
+	 */
 	private String cedulaPaciente;
+	
+	/**
+	 * Documento de identificacion del medico
+	 */
 	private String docmedico;
+	
+	/**
+	 * nit del medico
+	 */
+	private String nitmedico;
+	
+	/**
+	 * Guarda fechas a buscar
+	 */
 	private Date fechaSeleccion;
+	
+	/**
+	 * enumeracion de los motivos de las consultas
+	 */
 	private MotivoConsultaEnum motivoConsulta;
+	
+	/**
+	 * Enumeracion de los diferentes tipos de procedimiento a realizar en una consulta
+	 */
 	private TipoProcedimietoEnum procedimientoConsulta;
-	private boolean unidad;
-	private String estadoConsulta;
-	private String horaCita;
+	
+	/**
+	 * enumeracion de los diferentes tipos esx
+	 */
+	private TipoDocumentoEnum tipoDocumento;
+	public void setTipoDocumento(TipoDocumentoEnum tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
 
+	private boolean unidad;
+	private String horaCita;
+	private String nombreMedico;
+	List<Consulta> consultasMedico;
+	List<Consulta> consulPacienteCita;
+	
 	private EntityManager em;
 
 	public AsignarCitasBean() {
@@ -40,22 +82,21 @@ public class AsignarCitasBean {
 	 * 
 	 * @return
 	 */
-	public String crearCrita() {
-		
-		
-		//al string de la hora seleccionada para la cita hacer split para
-		//separar las horas de los minutos
+	public String crearCita() {
+
+		// al string de la hora seleccionada para la cita hacer split para
+		// separar las horas de los minutos
 		String[] campos = horaCita.split(":");
-		//el campo en cero guarda las horas
+		// el campo en cero guarda las horas
 		int hora = Integer.parseInt(campos[0]);
-		//el campo en 1 guarda los minutos
+		// el campo en 1 guarda los minutos
 		int minutos = Integer.parseInt(campos[1]);
-		
-		Calendar fechaSelect= Calendar.getInstance();
-        fechaSelect.setTime(fechaSeleccion);
-        fechaSelect.set(Calendar.HOUR, hora);
-        fechaSelect.set(Calendar.MINUTE, minutos);
-	    fechaSelect.set(Calendar.SECOND, 0);
+
+		Calendar fechaSelect = Calendar.getInstance();
+		fechaSelect.setTime(fechaSeleccion);
+		fechaSelect.set(Calendar.HOUR, hora);
+		fechaSelect.set(Calendar.MINUTE, minutos);
+		fechaSelect.set(Calendar.SECOND, 0);
 
 		// buscar el paciente que quiere asignar cita
 		Paciente paciente = em.find(Paciente.class, cedulaPaciente);
@@ -64,45 +105,53 @@ public class AsignarCitasBean {
 		if (paciente != null) {
 
 			// buscar el medico que se desea para la cita
-			Medico medico = em.find(Medico.class, docmedico);
+			Medico medico = em.find(Medico.class, nitmedico);
 
 			// validar que el medico si exista
 			if (medico != null) {
-			
+
 				// consulta para buscar por registro del medico sus consultas
 				Query query = em
 						.createNamedQuery(Consulta.FIND_CONSULTA_BY_MEDICO_AND_FECHAS);
 				query.setParameter(Consulta.PARAMETRO_REGISTRO_MEDICO,
 						medico.getRegistroMedico());
-				
-				query.setParameter(Consulta.PARAMETRO_MENOR_FECHA, fechaSelect.getTime());
-				query.setParameter(Consulta.PARAMETRO_MAYOR_FECHA, fechaSelect.getTime());
 
+				query.setParameter(Consulta.PARAMETRO_MENOR_FECHA,
+						fechaSelect.getTime());
+				query.setParameter(Consulta.PARAMETRO_MAYOR_FECHA,
+						fechaSelect.getTime());
 
 				// lista para guardar las consultas encontradas
 				List<Consulta> consultasMedico = query.getResultList();
 
-				//validar que tenga consultas de no tenerlas crea la cita sin dificultades
+				// validar que tenga consultas de no tenerlas crea la cita sin
+				// dificultades
 				if (consultasMedico.size() > 0) {
 
-							FacesContext
-									.getCurrentInstance()
-									.addMessage(
-											null,
-											new FacesMessage(
-													FacesMessage.SEVERITY_ERROR,
-													"la cita ya fue asignada con otro paciente",
-													null));
+					FacesContext
+							.getCurrentInstance()
+							.addMessage(
+									null,
+									new FacesMessage(
+											FacesMessage.SEVERITY_ERROR,
+											"la cita ya fue asignada con otro paciente",
+											null));
 
-
-				}else{
-					//si no hay consultas con el medico 
+				} else {
+					// si no hay consultas con el medico
 					em.getTransaction().begin();
-					Consulta consultaNueva = new Consulta(fechaSelect.getTime(), paciente, medico,
-					motivoConsulta, procedimientoConsulta);
+
+					Consulta consultaNueva = new Consulta(
+							fechaSelect.getTime(), paciente, medico,
+							MotivoConsultaEnum.PRIMERA_VEZ,TipoProcedimietoEnum.EXAMEN);
 
 					em.persist(consultaNueva);
 					em.getTransaction().commit();
+
+					FacesContext.getCurrentInstance().addMessage(
+							null,
+							new FacesMessage(FacesMessage.SEVERITY_INFO,
+									"se ha creado la cita", null));
 				}
 			}
 
@@ -110,14 +159,48 @@ public class AsignarCitasBean {
 		return null;
 	}
 
+	public List<SelectItem> getMotivosConsulta() {
+
+		List<SelectItem> lista = new ArrayList();
+
+		for (MotivoConsultaEnum motivoConsultaEnum : MotivoConsultaEnum
+				.values()) {
+			lista.add(new SelectItem(motivoConsultaEnum, motivoConsultaEnum
+					.getDescripcion()));
+		}
+
+		return lista;
+	}
+
+	public List<SelectItem> getTipoProcedimiento() {
+
+		List<SelectItem> lista = new ArrayList();
+
+		for (TipoProcedimietoEnum tipo : TipoProcedimietoEnum.values()) {
+			lista.add(new SelectItem(tipo, tipo.getDescripcion()));
+		}
+
+		return lista;
+	}
+
+	public List<SelectItem> getTipoDocumento() {
+
+		List<SelectItem> lista = new ArrayList();
+
+		for (TipoDocumentoEnum tipo : TipoDocumentoEnum.values()) {
+			lista.add(new SelectItem(tipo, tipo.getDescripcion()));
+		}
+
+		return lista;
+	}
+	
 	public List<SelectItem> getMedicos() {
-		List<Medico> medicos = em.createNamedQuery(Medico.FIND_ALL)
-				.getResultList();
+
+		List<Medico> medicos = em.createNamedQuery(Medico.FIND_ALL).getResultList();
 		List<SelectItem> medicItems = new ArrayList();
 
 		for (Medico medico : medicos) {
-			SelectItem medicoItem = new SelectItem(medico.getDocumento(),
-					medico.toString());
+			SelectItem medicoItem = new SelectItem(medico.getDocumento(),medico.getNombre());
 			medicItems.add(medicoItem);
 		}
 
@@ -130,27 +213,45 @@ public class AsignarCitasBean {
 	 * 
 	 * @return
 	 */
-	public List<SelectItem> getHorariosMedico() {
-
-		Medico medico = em.find(Medico.class, docmedico);
-
-		Query query = em
-				.createNamedQuery(Consulta.FIND_CONSULTA_BY_MEDICO_AND_FECHAS);
-		query.setParameter(Consulta.PARAMETRO_REGISTRO_MEDICO, docmedico);
-
-		List<Consulta> consultasMedico = query.getResultList();
-		List<SelectItem> horasItems = new ArrayList();
-
+	public String mostrarHorariosMedico() {
 		
-		for (Consulta consul : consultasMedico) {
-			SelectItem fechoras = new SelectItem(consul.getFechaHora());
-			horasItems.add(fechoras);
+		Calendar mayorfecha=Calendar.getInstance();
+		mayorfecha.setTime(fechaBuscar);
+		mayorfecha.set(Calendar.HOUR, 20);
+		mayorfecha.set(Calendar.MINUTE, 0);
+			
+		Calendar menorfecha=Calendar.getInstance();
+		menorfecha.setTime(fechaBuscar);
+		menorfecha.set(Calendar.HOUR, 6);
+		menorfecha.set(Calendar.MINUTE, 0);
+		
+		Query query = em.createNamedQuery(Consulta.FIND_CONSULTA_BY_MEDICO_AND_FECHAS);
+		query.setParameter(Consulta.PARAMETRO_REGISTRO_MEDICO, nitmedico);
+		query.setParameter(Consulta.PARAMETRO_MAYOR_FECHA,mayorfecha.getTime());
+		query.setParameter(Consulta.PARAMETRO_MENOR_FECHA,menorfecha.getTime());
+
+		consultasMedico = query.getResultList();
+		
+		if(consultasMedico.size()==0){
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"El medico no tiene consultas en esta fecha", null));
 		}
 
-		return horasItems;
+		return null;
 	}
 	
-	
+	public String eliminarCita(){
+		
+		Query quero = em.createNamedQuery(Consulta.FIND_CONSULTA_BY_NUMERO_Y_TIPO_DOCUMENTO);
+		quero.setParameter(Consulta.PARAMETRO_NUMERO_DOCUMENTO, cedulaPaciente);
+		quero.setParameter(Consulta.PARAMETRO_TIPO_DOCUMENTO, tipoDocumento);
+		consulPacienteCita = quero.getResultList();
+		
+		if(consulPacienteCita.size() == 0){
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "El paciente no tiene citas", null));
+		}
+		
+		return null;
+	}
 
 	/*
 	 * Getters and setters
@@ -159,6 +260,7 @@ public class AsignarCitasBean {
 	public String getCedulaPaciente() {
 		return cedulaPaciente;
 	}
+
 
 	public void setCedulaPaciente(String cedulaPaciente) {
 		this.cedulaPaciente = cedulaPaciente;
@@ -197,12 +299,28 @@ public class AsignarCitasBean {
 		this.unidad = unidad;
 	}
 
-	public String getEstadoConsulta() {
-		return estadoConsulta;
+	public String getHoraCita() {
+		return horaCita;
 	}
 
-	public void setEstadoConsulta(String estadoConsulta) {
-		this.estadoConsulta = estadoConsulta;
+	public void setHoraCita(String horaCita) {
+		this.horaCita = horaCita;
+	}
+
+	public String getNombreMedico() {
+		return nombreMedico;
+	}
+
+	public void setNombreMedico(String nombreMedico) {
+		nombreMedico = nombreMedico;
+	}
+
+	public EntityManager getEm() {
+		return em;
+	}
+
+	public void setEm(EntityManager em) {
+		this.em = em;
 	}
 
 	public String getDocmedico() {
@@ -213,12 +331,36 @@ public class AsignarCitasBean {
 		this.docmedico = docmedico;
 	}
 
-	public String getHoraCita() {
-		return horaCita;
+	public String getNitmedico() {
+		return nitmedico;
 	}
 
-	public void setHoraCita(String horaCita) {
-		this.horaCita = horaCita;
+	public void setNitmedico(String nitmedico) {
+		this.nitmedico = nitmedico;
 	}
+
+	public Date getFechaBuscar() {
+		return fechaBuscar;
+	}
+
+	public void setFechaBuscar(Date fechaBuscar) {
+		this.fechaBuscar = fechaBuscar;
+	}
+
+	public List<Consulta> getConsultasMedico() {
+		return consultasMedico;
+	}
+
+	public void setConsultasMedico(List<Consulta> consultasMedico) {
+		this.consultasMedico = consultasMedico;
+	}
+
+	public List<Consulta> getConsulPacienteCita() {
+		return consulPacienteCita;
+	}
+
+	public void setConsulPacienteCita(List<Consulta> consulPacienteCita) {
+		this.consulPacienteCita = consulPacienteCita;
+	}	
 
 }
